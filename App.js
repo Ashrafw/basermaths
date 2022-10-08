38a7eaf5c96c6c2076c04754c500b3e635c06598
Ashraf W <ashrafw@Ashrafs-MacBook-Pro.local>
2022-10-08 08:49:03 +0100

    faqs

--- App.js
+++ App.js
@@ -1,28 +1,33 @@
-const cards = document.querySelectorAll('.newCard')
-const pastelOne = document.querySelector('.pastel-one')
-const cardOne = document.querySelector('.card-one')
-const cardTwo = document.querySelector('.card-two')
-const cardThree = document.querySelector('.card-three')
+// const cards = document.querySelectorAll(".newCard");
+// const pastelOne = document.querySelector(".pastel-one");
+// const cardOne = document.querySelector(".card-one");
+// const cardTwo = document.querySelector(".card-two");
+// const cardThree = document.querySelector(".card-three");
 
-const options={threshold:0}
+// const options = { threshold: 0 };
 
-    console.log(pastelOne)
+// console.log(pastelOne);
 
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach(entry => {
-        console.log(entry.target.classList)
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry.target.classList);
 
-            entry.target.classList.toggle( 'active',entry.isIntersecting)
-            if(entry.isIntersecting) observer.unobserve(entry.target)
-    })
-    
-},options)
+//     entry.target.classList.toggle("active", entry.isIntersecting);
+//     if (entry.isIntersecting) observer.unobserve(entry.target);
+//   });
+// }, options);
 
-cards.forEach(card => {
-    observer.observe(card)
-})
+// cards.forEach((card) => {
+//   observer.observe(card);
+// });
 
+const toggleBtn = document.querySelectorAll(".faq-card");
 
+toggleBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("active"); //only toggles the parent node which is the '.faq-card' here
+  });
+});
 // const observer= new IntersectionObserver((entry)=>{
 //     console.log(entry[0].target)
 //     entry[0].target.classList.toggle('leftin',entry.isIntersecting)
@@ -31,31 +36,29 @@ cards.forEach(card => {
 
 // observer.observe(pastelOne)
 
+// const moveObserverOne = new IntersectionObserver((entry) => {
+//   if (entry[0].isIntersecting) {
+//     entry[0].target.style.animation = `movingLeftIn 1.5s ease-out`;
+//     observer.unobserve(entry[0].target);
+//   }
+// });
 
-const moveObserverOne = new IntersectionObserver((entry) => {
-      if (entry[0].isIntersecting) {
-      entry[0].target.style.animation = `movingLeftIn 1.5s ease-out`;
-      observer.unobserve(entry[0].target)
-    } 
-});
+// moveObserverOne.observe(cardOne);
 
-moveObserverOne.observe(cardOne);
+// const moveObserverTwo = new IntersectionObserver((entry) => {
+//   if (entry[0].isIntersecting) {
+//     entry[0].target.style.animation = `movingTopIn 1.5s ease-out`;
+//     observer.unobserve(entry[0].target);
+//   }
+// });
 
-const moveObserverTwo = new IntersectionObserver((entry) => {
-    if (entry[0].isIntersecting) {
-      entry[0].target.style.animation = `movingTopIn 1.5s ease-out`;
-      observer.unobserve(entry[0].target)
+// moveObserverTwo.observe(cardTwo);
 
-    }
-});
+// const moveObserverThree = new IntersectionObserver((entry) => {
+//   if (entry[0].isIntersecting) {
+//     entry[0].target.style.animation = `movingRightIn 1.5s ease-out`;
+//     observer.unobserve(entry[0].target);
+//   }
+// });
 
-moveObserverTwo.observe(cardTwo);
-
-const moveObserverThree = new IntersectionObserver((entry) => {
-      if (entry[0].isIntersecting) {
-      entry[0].target.style.animation = `movingRightIn 1.5s ease-out`;
-      observer.unobserve(entry[0].target)
-    } 
-});
-
-moveObserverThree.observe(cardThree);
+// moveObserverThree.observe(cardThree);
